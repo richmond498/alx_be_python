973ae7df59855978a868a19ff93b63083ac6d2ed
richmond adusei <richmond.nana2000@gmail.com>
2025-11-05 14:46:03 -0200

    basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+summ = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("Addition of 10 and 5 is" , summ)
+print("Subtraction of 10 and 5 is", difference)
+print("Multiplication of 10 and 5 is", product)
