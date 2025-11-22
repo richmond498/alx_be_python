8c840a58c73d35572b9591a5a411ea9592696745
richmond adusei <richmond.nana2000@gmail.com>
2025-11-22 18:36:01 -0200

    added main.py and operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        num1 + num2
+    elif operation == "subtract":
+        num1 - num2
+    elif operation == "multiply":
+        num1 * num2 
+    elif operation == "divide":
+        num1/num2
+
+    
