f3d11fa7dfd17278afca35095c1402f4d560dd1b
richmond adusei <richmond.nana2000@gmail.com>
2025-11-22 19:44:07 -0200

    chages to first file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
-        num1 + num2
+        return num1 + num2
     elif operation == "subtract":
-        num1 - num2
+        return num1 - num2
     elif operation == "multiply":
-        num1 * num2 
+        return num1 * num2 
     elif operation == "divide" and num2 == 0:
         print(f"num2 can not be zero")
     elif operation == "divide":
-        num1/num2
+        return num1/num2
 
     
