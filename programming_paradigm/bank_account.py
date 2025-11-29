541e4dbd2f7593a6febe84f081be9c278d163910
richmond adusei <richmond.nana2000@gmail.com>
2025-11-29 10:31:30 +0000

    changes to bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,4 +17,4 @@ class BankAccount:
 
     def display_balance(self):
         """Print the current account balance."""
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
